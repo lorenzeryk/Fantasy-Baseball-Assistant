39bc39b9840f0bce1d939a05cea02bb9928dee35
Eryk Lorenz <eryklorenz@mines.edu>
2023-03-30 21:25:32 -0600

    Initial Commit

---
+++ Fantasy-Baseball-Assistant/Fantasy-Baseball-Assistant/Fantasy_Baseball_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fantasy_Baseball_AssistantApp.swift
+//  Fantasy-Baseball-Assistant
+//
+//  Created by Eryk Lorenz on 3/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Fantasy_Baseball_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
